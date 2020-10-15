c90eee34c6bc3a81b1fa307c1f492c2452297e1b
deepakkumar00 <deepakkumarcse001@gmail.com>
2020-10-15 12:20:04 +0530

    Added Selection Sort

---
+++ sorting/selection_sort.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, i, imin, a[100];
+	cout << "Size of unsorted array: ";
+	cin >> n;
+	cout << "Enter unsorted array: \n";
+	for(i = 0; i < n; i++)
+		cin>>a[i];
+	for(i = 0; i < n; i++)
+	{
+		int min = a[i];
+		for(int j = i; j < n-1; j++)
+		{
+			if(a[j+1] < min)
+			{
+				min = a[j+1];
+				imin = j + 1;
+			}
+		}
+		int tmp = a[i];
+		a[i] = min;
+		a[imin]= tmp;
+	}
+	cout<<"Sorted Array: ";
+	for(i = 0; i < n; i++)
+		cout<<a[i]<<" ";
+	return 0;
+}
